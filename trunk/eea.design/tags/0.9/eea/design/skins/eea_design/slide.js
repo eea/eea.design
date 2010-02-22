9367b6377e476503a5ee3ef07a336594b408f01e
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-02-22 15:00:00 +0000

    Tagged eea.design 0.9.

---
+++ trunk/eea.design/tags/0.9/eea/design/skins/eea_design/slide.js
@@ -0,0 +1,50 @@
+(function($) {
+    $(document).ready(function() {
+        function panel(i) {
+            var a = $(this);
+            var tooltip = $('#tip-' + a.parent().attr('id'));
+            if (tooltip.length > 0) {
+                a.attr("title","").attr("href", "#");
+
+                a.mouseover(function() {
+                    a.parent().addClass("selected");
+                });
+
+                // the tooltip panel should have the id in form of
+                // tip-SITEACTION-ID
+                a.tooltip({
+                    tip: tooltip[0],
+                    position: 'bottom center',
+                    offset: [0, 0],
+                    delay: 1000,
+                    events: {
+                        tooltip: 'mouseover' 
+                    },
+                    onShow: function(e) {
+                        a.parent().addClass("selected");
+                    },
+                    onBeforeHide: function(e, i) {
+                        a.parent().removeClass("selected");
+                    }
+                });
+            }
+
+            // remove panel if user clicks outside it
+            $(document).click(function(e) {
+                var target = $(e.target);
+                if (!target.is('#cross-site-top .panel') && !target.parents('#cross-site-top .panel').length) {
+                    $('#cross-site-top .panel').fadeOut('fast');
+                }
+            });
+        }
+        $("#portal-siteactions a").each(panel);
+        $("#portal-externalsites a").each(panel);
+
+        // since we use the standard events_portlet, we can't place the
+        // submit-event-link in its footer initially
+        var footer = $("#tip-siteaction-events .portletFooter");
+        var submitLink = $("#tip-siteaction-events #submit-event-link");
+        submitLink.remove().css('margin-right', '0.5em');
+        footer.prepend(submitLink);
+    });
+})(jQuery);
