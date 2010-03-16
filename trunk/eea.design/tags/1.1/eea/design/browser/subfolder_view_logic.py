4c346e106aa7a205dd218dcaf242c1f45fccc350
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-03-16 13:02:36 +0000

    Tagged eea.design 1.1.

---
+++ trunk/eea.design/tags/1.1/eea/design/browser/subfolder_view_logic.py
@@ -0,0 +1,38 @@
+from Products.CMFPlone import Batch
+from Products.Five import BrowserView
+
+
+def _get_contents(folder_brain):
+    """Get contents of folderish brain (cachable list/dict format)"""
+    if folder_brain.portal_type == 'Folder':
+        brains = folder_brain.getObject().getFolderContents()
+    elif folder_brain.portal_type in ['Topic', 'RichTopic']:
+        brains = folder_brain.getObject().queryCatalog()
+    return [{
+        'title': brain.Title,
+        'description': brain.Title,
+        'url': brain.getURL(),
+        'portal_type': brain.portal_type,
+    } for brain in brains]
+
+
+class SubFolderView(BrowserView):
+    """ View that shows the contents of all subfolders in the context folder
+    """
+
+    def folder_contents(self, size_limit=10):
+        """Get the folderish items in cachable list/dict format"""
+        size_limit = int(self.request.get('size_limit', size_limit))
+        ret = []
+        for brain in self.context.getFolderContents():
+            if brain.portal_type in ['Folder', 'Topic', 'RichTopic']:
+                contents = _get_contents(brain)
+                ret.append({
+                    'title': brain.Title,
+                    'description': brain.Description,
+                    'url': brain.getURL(),
+                    'portal_type': brain.portal_type,
+                    'contents': contents[:size_limit],
+                    'has_more': len(contents) > size_limit,
+                })
+        return ret
---
+++ trunk/eea.design/tags/1.1/eea/design/skins/eea_design/eea-fancybox.js
@@ -0,0 +1,18 @@
+// Integration JS between fancybox and EEA
+$(document).ready(function() {
+    $('.fancybox').fancybox();
+    $('.gallery-fancybox').each(function() {
+        var href = $(this).attr('href') + "/gallery_fancybox_view";
+        $(this).attr('href', href);
+        $(this).fancybox({
+            type: 'iframe',
+            padding: 0,
+            margin: 0,
+            width: 780,
+            height: 580,
+            scrolling: 'no',
+            autoScale: false,
+            autoDimensions: false
+        });
+    });
+});
