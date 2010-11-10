bd41629c75fc2d02898cc793f412a34413c1e01c
vincisas <vincisas@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-11-10 12:59:57 +0000

    Tagged eea.design 2.5.

---
+++ trunk/eea.design/tags/2.5/eea/design/browser/soer_frontpage.py
@@ -0,0 +1,113 @@
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+
+class SoerFrontpage(BrowserView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        utils = getToolByName(context, 'plone_utils')
+        if utils.isDefaultPage(context):
+            self.soer = context.aq_parent
+        else:
+            self.soer = context
+
+    def getMessages(self):
+        ret = []
+        catalog = getToolByName(self.context, 'portal_catalog')
+        brains = catalog({
+            'portal_type': 'SOERMessage',
+        })
+        for brain in brains:
+            text = brain.Description
+            if len(text) > 400:
+                text = text[:400] + '...'
+            ret.append({
+                'text': text,
+                'url': brain.getURL,
+            })
+        return ret
+
+    def getKeyFacts(self):
+        ret = []
+        catalog = getToolByName(self.context, 'portal_catalog')
+        brains = catalog({
+            'portal_type': 'SOERKeyFact',
+        })
+        for brain in brains:
+            text = brain.Description
+            if len(text) > 400:
+                text = text[:400] + '...'
+            ret.append({
+                'text': text,
+                'url': brain.getURL,
+            })
+        return ret
+
+    def getSoerTopics(self):
+        return [
+            {
+                'label': 'Climate Change',
+                'tags': 'climate change',
+            },
+            {
+                'label': 'Nature and biodiversity',
+                'tags': 'nature and biodiversity',
+            },
+            {
+                'label': 'Land use',
+                'tags': 'land use',
+            },
+            {
+                'label': 'Soil',
+                'tags': 'soil',
+            },
+            {
+                'label': 'Marine and coastal environment',
+                'tags': 'marine and coastal',
+            },
+            {
+                'label': 'Consumption',
+                'tags': 'consumption',
+            },
+            {
+                'label': 'Material resources and waste',
+                'tags': 'material resources, natural resources, waste',
+            },
+            {
+                'label': 'Freshwater',
+                'tags': 'freshwater quality, water resources',
+            },
+            {
+                'label': 'Air pollution',
+                'tags': 'air pollution',
+            },
+            {
+                'label': 'Urban environment',
+                'tags': 'urban environment',
+            },
+            {
+                'label': 'Global megatrends',
+                'tags': 'global megatrends',
+            },
+        ]
+
+    def getSoerLocations(self):
+        return [
+            {
+                'label': 'Country name',
+                'tags': 'country name',
+            },
+            {
+                'label': 'Former Yugoslav Republic of Macedonia',
+                'tags': 'macedonia',
+            },
+            {
+                'label': 'Europe',
+                'tags': '-',
+            },
+            {
+                'label': 'World',
+                'tags': 'world',
+            },
+        ]
