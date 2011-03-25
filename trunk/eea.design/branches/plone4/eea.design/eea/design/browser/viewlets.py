3c087fb0e7ff8922f34d67107dbfdb5f02a22778
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-03-25 16:46:48 +0000

    Refs #8192; added images from old package; fixed cross_top_panel, added and fixed frontpage; fixed footer & colophon;

--- trunk/eea.design/branches/plone4/eea.design/eea/design/browser/viewlets.py
+++ trunk/eea.design/branches/plone4/eea.design/eea/design/browser/viewlets.py
@@ -2,6 +2,6 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets import common
 
 class SiteActionViewlet(common.SiteActionsViewlet):
-    """A custom version of the path bar class
+    """A custom version of the site-actions class
     """
     render = ViewPageTemplateFile('templates/site_actions.pt')
