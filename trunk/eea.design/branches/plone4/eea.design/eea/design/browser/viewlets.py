e6db8114318971b007548a170b89d4192d3ac9fa
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-04-21 15:12:43 +0000

    Refs #8192; added sections.pt for cms; path_bar only for anonymoys; & more documentation to eea.designdependencies wiki

--- trunk/eea.design/branches/plone4/eea.design/eea/design/browser/viewlets.py
+++ trunk/eea.design/branches/plone4/eea.design/eea/design/browser/viewlets.py
@@ -3,7 +3,7 @@ from plone.app.layout.viewlets import common, content
 from zope.component import getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 
-class SiteActionViewlet(common.SiteActionsViewlet):
+class SiteActionsViewlet(common.SiteActionsViewlet):
     """A custom version of the site-actions class
     """
     render = ViewPageTemplateFile('templates/site_actions.pt')
@@ -13,10 +13,10 @@ class LogoViewlet(common.LogoViewlet):
     """
     render = ViewPageTemplateFile('templates/logo.pt')
 
-class GlobalSectionsViewlet(common.GlobalSectionsViewlet):
+class GlobalSectionsCMSViewlet(common.GlobalSectionsViewlet):
     """A custom version of the global-sections class
     """
-    render = ViewPageTemplateFile('templates/sections.pt')
+    render = ViewPageTemplateFile('templates/sections_cms.pt')
 
 class DocumentActionsViewlet(content.DocumentActionsViewlet):
     """A custom version of the document-actions class
