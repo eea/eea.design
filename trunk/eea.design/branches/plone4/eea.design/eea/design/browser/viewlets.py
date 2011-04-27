c50bfa189eb3bea73773ff60cc12b0b773d9e3e1
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-04-27 08:46:30 +0000

    Refs #8192; added header.pt logic with htmlhead links& ie7 conditional css

--- trunk/eea.design/branches/plone4/eea.design/eea/design/browser/viewlets.py
+++ trunk/eea.design/branches/plone4/eea.design/eea/design/browser/viewlets.py
@@ -7,12 +7,12 @@ from cgi import escape
 from Products.CMFPlone.utils import safe_unicode
 
 class SiteActionsViewlet(common.SiteActionsViewlet):
-    """A custom version of the site-actions class
+    """A custom version of the site-actions viewlet
     """
     render = ViewPageTemplateFile('templates/site_actions.pt')
 
 class LogoViewlet(common.LogoViewlet):
-    """A custom version of the site-actions class
+    """A custom version of the logo viewlet
     """
     render = ViewPageTemplateFile('templates/logo.pt')
 
@@ -22,7 +22,7 @@ class GlobalSectionsCMSViewlet(common.GlobalSectionsViewlet):
     render = ViewPageTemplateFile('templates/sections_cms.pt')
 
 class TitleViewlet(common.TitleViewlet):
-    """A custom version of the site-actions class
+    """A custom version of the title viewlet
     """
     index = ViewPageTemplateFile('templates/title.pt')
     def update(self):
@@ -37,30 +37,42 @@ class TitleViewlet(common.TitleViewlet):
 
 
 class DocumentActionsViewlet(content.DocumentActionsViewlet):
-    """A custom version of the document-actions class
+    """A custom version of the document-actions viewlet
     """
     render = ViewPageTemplateFile('templates/document_actions.pt')
 
 
 class DocumentBylineViewlet(content.DocumentBylineViewlet):
-    """A custom version of the document-byline class
+    """A custom version of the document-byline viewlet
     """
     render = ViewPageTemplateFile('templates/document_byline.pt')
 
 class FooterPortletsViewlet(common.ViewletBase):
+    """A modified footer viewlet to contain portlet information
+    """
     render = ViewPageTemplateFile('templates/footer.pt')
 
     def update(self):
         """
         Define everything we want to call in the template
         """
-        context_state = getMultiAdapter((self.context, self.request), name=u'plone_context_state')
-        self.manageUrl =  '%s/@@manage-portlets-footer' % context_state.view_url()
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        self.manageUrl =  '%s/@@manage-portlets-footer' % \
+                                        context_state.view_url()
 
-        ## This is the way it's done in plone.app.portlets.manager, so we'll do the same
+        # This is the way it's done in plone.app.portlets.manager
         mt = getToolByName(self.context, 'portal_membership')
-        self.canManagePortlets = mt.checkPermission('Portlets: Manage portlets', self.context)
+        self.canManagePortlets = mt.checkPermission('Portlets: Manage portlets',
+                                                    self.context)
 
 
 class SearchViewlet(links.SearchViewlet):
+    """A custom version of the links-search viewlet
+    """
     _template = ViewPageTemplateFile('templates/links_search.pt')
+
+class NavigationViewlet(links.NavigationViewlet):
+    """A custom version of the links-navigation viewlet
+    """
+    _template = ViewPageTemplateFile('templates/links_navigation.pt')
