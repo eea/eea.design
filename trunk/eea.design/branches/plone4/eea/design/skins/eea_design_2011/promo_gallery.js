c6952f22610f26242d71a97a13e2c47b220d2868
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-07-25 10:05:54 +0000

    Refs #8410; normalize size of gallery pictures to 448x320 to prevent images from appearing with different sizes; made the gallery size bigger to 420px

--- trunk/eea.design/branches/plone4/eea/design/skins/eea_design_2011/promo_gallery.js
+++ trunk/eea.design/branches/plone4/eea/design/skins/eea_design_2011/promo_gallery.js
@@ -1,22 +1,31 @@
 // call of the promo_gallery_script gallery
 (function($) {
     $(function() { 
+        
         var buttons = { previous:$('#prev-promo') ,
 						next:$('#next-promo') };
-        $('#promotions').lofJSidernews( {       interval            : 9000,
-                                                direction           : 'opacity',
-												duration            : 1500,
-                                                wapperSelector      : '#promo-gallery-content',
-                                                navItemsSelector    : '#promo-navigator li',
-                                                navOuterSelector    : '#navigator-outer',
-	                                            isPreloaded			: false,
-			                                    navigatorHeight		: 80,
-			                                    navigatorWidth		: 170,
-											    auto                : true,
-                                                caption             : 'promo-item-desc',
-                                                opacityClass        : 'lof-opacity',  
-                                                buttons             : buttons 
-                                         } );
+        
+        $("#promo-gallery-content img").each(
+                function() {
+                    this.width = "448";
+                    this.height = "320";
+                }
+        ); 
+        
+        $('#promotions').lofJSidernews({     interval            : 9000,
+                                             direction           : 'opacity',
+											 duration            : 1500,
+                                             wapperSelector      : '#promo-gallery-content',
+                                             navItemsSelector    : '#promo-navigator li',
+                                             navOuterSelector    : '#navigator-outer',
+	                                         isPreloaded		 : false,
+			                                 navigatorHeight	 : 80,
+			                                 navigatorWidth		 : 170,
+											 auto                : true,
+                                             caption             : 'promo-item-desc',
+                                             opacityClass        : 'lof-opacity',  
+                                             buttons             : buttons 
+                                        });
 
     })
 })(jQuery);
