236f657a12e8c1bf53848a572f95475b6ecc5d72
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-02 11:58:29 +0000

    Refs #8410; changed articles logic to use listings; fixed loading of video-popup view once you load new videos through ajax; clean site-address to avoid accordion script malfunction when clicking  links on the frontpage that have a href of #

--- trunk/eea.design/branches/plone4/eea/design/skins/eea_design_2011/accordion_gallery.js
+++ trunk/eea.design/branches/plone4/eea/design/skins/eea_design_2011/accordion_gallery.js
@@ -9,12 +9,16 @@
                 var sel_value = y[x].value;
                 var cur_tab_val = $("#tabs a.current").text().toLowerCase();
                 var site_address = window.location.href;
+                // remove any # or #items links from the link address 
+                site_address = site_address.replace(/#\w*/, '')
                 var news = $(".highlights").filter( function(index) { 
                      return this.style.display === "block"; 
                 });
                 var address = site_address + cur_tab_val + "_gallery_macro";
                 news.load( address, {topic: sel_value, tab: cur_tab_val },  function(html) {
-                    news.tabs("div.highlightMiddle", {tabs: 'div.panel', effect: 'slide'});
+                    if (cur_tab_val === "multimedia") {
+                        $.getScript(site_address + "eea-mediacentre.js");
+                    }
                 });
             }
         );
