396a22e3b9c0f0532ad94ab82e3fe6cbe2af78ee
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-05 14:59:38 +0000

    Refs #8410; added switching of themes for what's new gallery

--- trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_js/accordion_gallery.js
+++ trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_js/accordion_gallery.js
@@ -23,19 +23,66 @@
                      return this.style.display === "block"; 
                 });
                 var address = site_address + cur_tab_val + "_gallery_macro";
-                var no_results = $("<p>No results are available for this topic</p>");
-                news.load( address, {topic: sel_value, tab: cur_tab_val },  function(html) {
+                var no_results = $("<div class='portalMessage informationMessage'><p>No results are available for this topic</p></div>");
+                var gallery_ajax = $(".gallery-ajax", news);
+                var layout_selection = $('.gallery-layout-selection a', news)[0];
+                gallery_ajax.load( address, {topic: sel_value, tab: cur_tab_val },  function(html) {
                     if (html.length > 1) {
+                        if (layout_selection.className === "list-layout active-list"){
+                            gallery_ajax.find('.gallery-album').addClass('hiddenStructure'); 
+                        }
+                        else {
+                            gallery_ajax.find('.gallery-listing').addClass('hiddenStructure'); 
+                        
+                        }
                         if (cur_tab_val === "multimedia") {
                             $.getScript(site_address + "eea-mediacentre.js");
                         }
                     }
                     else {
-                       no_results.appendTo(news);
+                       no_results.appendTo(gallery_ajax);
                     }
+                    
                 });
             }
         );
+
+        // selection of folder_summary_view or atct_album_view
+        var layout_links = $(".gallery-layout-selection a");
+        layout_links.filter( function() { return this.className === '';}).remove();        
+        var filtered_links = layout_links.filter( function() { return this.className != '';}); 
+        var listing_layout = $(".gallery-listing");
+        filtered_links.click( function(e) {
+             var $this = $(this);
+             var $parent = $this.parent();
+             var $ajax = $parent.next();
+             var $hidden_gallery = $ajax.find(".hiddenStructure");
+             var listing = $ajax.find('.gallery-listing');
+             var album = $ajax.find('.gallery-album');
+             var next = $this.siblings().first();
+             var link_class = $this[0].className;
+             
+             if ( link_class === "list-layout active-list" || link_class === "album-layout active-album") {
+                 return false;
+             };
+
+            if (link_class == "list-layout") {
+                album.slideUp();
+                listing.slideDown();
+                $hidden_gallery.removeClass("hiddenStructure");
+                $this.toggleClass("active-list");
+                next.toggleClass("active-album");
+                return false;
+            }
+            else {
+                listing.slideUp();
+                album.slideDown();
+                $hidden_gallery.removeClass("hiddenStructure");
+                $this.toggleClass("active-album");
+                next.toggleClass("active-list");
+                return false;
+            }
+        });
     });
 
 })(jQuery);
