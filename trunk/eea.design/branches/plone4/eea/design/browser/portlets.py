a0f5ca0e3f10a027a1c7c9fbec37edc349dd4cd5
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-10 15:41:00 +0000

    Refs #8361   - Cleanup navigation portlet related code as it should be handled by Products.NavigationManager package.

--- trunk/eea.design/branches/plone4/eea/design/browser/portlets.py
+++ trunk/eea.design/branches/plone4/eea/design/browser/portlets.py
@@ -1,4 +1,5 @@
-from plone.app.portlets.portlets.navigation import Renderer
+""" EEA Portlets
+"""
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import adapts
 from zope.interface import Interface
@@ -6,22 +7,6 @@ from zope.publisher.interfaces.browser import IBrowserView
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.app.portlets.manager import ColumnPortletManagerRenderer
 from eea.design.browser.interfaces import IFooterPortletManager
-
-class CMSNavigationPortlet(Renderer):
-    """
-    A renderer for the CMS footer portlet
-    """
-    _template = ViewPageTemplateFile("templates/navigation.pt")
-    recurse = ViewPageTemplateFile("templates/navigation_recurse_cms.pt")
-
-
-class EEANavigationPortlet(Renderer):
-    """
-    A renderer for the public footer portlet
-    """
-    _template = ViewPageTemplateFile("templates/navigation.pt")
-    recurse = ViewPageTemplateFile("templates/navigation_recurse.pt")
-
 
 class EEAFooterPortletRenderer(ColumnPortletManagerRenderer):
     """
