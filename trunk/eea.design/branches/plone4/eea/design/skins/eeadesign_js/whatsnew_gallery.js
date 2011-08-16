6e15a3b7659754c6ef288907bde9919fdf9f7ff8
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-16 12:02:28 +0000

    Fixes to be valid HTML5, refs #8429

--- trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_js/whatsnew_gallery.js
+++ trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_js/whatsnew_gallery.js
@@ -1,12 +1,12 @@
 (function($) {
-    $(function() { 
+    $(function() {
         $("#highlights-high, #highlights-middle").tabs("div.highlightMiddle", {tabs: 'div.panel', effect: 'slide'});
         $("ul#tabs").tabs("> .highlights");
 
         var site_address = window.location.href;
-        // remove any # or #items links from the site link address 
+        // remove any # or #items links from the site link address
         site_address = site_address.replace(/#\w*/, '');
-        
+
         var datamaps = $("#datamaps-highlights");
         var data_address  = site_address + "data-and-maps" + " " + ".photoAlbumEntry";
         datamaps.load(data_address);
@@ -19,21 +19,21 @@
                     y = this.options;
                 var sel_value = y[x].value;
                 var cur_tab_val = $("#tabs a.current").text().toLowerCase();
-                var news = $(".highlights").filter( function(index) { 
-                     return this.style.display === "block"; 
+                var news = $(".highlights").filter( function(index) {
+                     return this.style.display === "block";
                 });
                 var address = site_address + cur_tab_val + "_gallery_macro";
                 var no_results = $("<div class='portalMessage informationMessage'><p>No results are available for this topic</p></div>");
                 var gallery_ajax = $(".gallery-ajax", news);
-                var layout_selection = $('.gallery-layout-selection a', news)[0];
+                var layout_selection = $('.gallery-layout-selection li a', news)[0];
                 gallery_ajax.load( address, {topic: sel_value, tab: cur_tab_val },  function(html) {
                     if (html.length > 1) {
                         if (layout_selection.className === "list-layout active-list"){
-                            gallery_ajax.find('.gallery-album').addClass('hiddenStructure'); 
+                            gallery_ajax.find('.gallery-album').addClass('hiddenStructure');
                         }
                         else {
-                            gallery_ajax.find('.gallery-listing').addClass('hiddenStructure'); 
-                        
+                            gallery_ajax.find('.gallery-listing').addClass('hiddenStructure');
+
                         }
                         if (cur_tab_val === "multimedia") {
                             $.getScript(site_address + "eea-mediacentre.js");
@@ -42,15 +42,15 @@
                     else {
                        no_results.appendTo(gallery_ajax);
                     }
-                    
+
                 });
             }
         );
 
         // selection of folder_summary_view or atct_album_view
-        var layout_links = $(".gallery-layout-selection a");
-        layout_links.filter( function() { return this.className === '';}).remove();        
-        var filtered_links = layout_links.filter( function() { return this.className != '';}); 
+        var layout_links = $(".gallery-layout-selection li a");
+        layout_links.filter( function() { return this.className === '';}).remove();
+        var filtered_links = layout_links.filter( function() { return this.className != '';});
         var listing_layout = $(".gallery-listing");
         filtered_links.click( function(e) {
              var $this = $(this);
@@ -61,7 +61,7 @@
              var album = $ajax.find('.gallery-album');
              var next = $this.siblings().first();
              var link_class = $this[0].className;
-             
+
              if ( link_class === "list-layout active-list" || link_class === "album-layout active-album") {
                  return false;
              };
