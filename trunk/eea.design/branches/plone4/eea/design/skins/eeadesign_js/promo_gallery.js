1851e3cd7a2880da0b1de786c0be5e96dd6d299e
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-30 07:38:59 +0000

    Refs #8444; moved promo_gallery_promo portlet navigation from vertical to horizontal position to better fit the smaller portal-column-two

--- trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_js/promo_gallery.js
+++ trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_js/promo_gallery.js
@@ -12,11 +12,13 @@
                 });
     
         var site_address = window.location.href.indexOf('themes') != '-1' ? true : undefined,
-            nav_height = site_address ? 50 : 80, // 80  
-            nav_width = site_address ? 65 : 170, // 90
+            nav_height = site_address ? 50 : 80,  
+            nav_width = site_address ? 65 : 170, 
             nav_items_selector = site_address ? "#promo-navigator-sm li" : "#promo-navigator li", 
             wrapper_selector = site_address ? "#promo-gallery-content-sm" : "#promo-gallery-content",
-            max_item_display = site_address ? 4 : 3;
+            max_item_display = site_address ? 5 : 3,
+            nav_position = site_address ? 'horizontal' : 'vertical',
+            nav_outer_selector = site_address ? "#navigator-outer-sm" : "#navigator-outer";
 
         var promotions = $('#promo-gallery-wrapper').lofJSidernews({
                                              interval            : 9000,
@@ -24,11 +26,12 @@
                                              duration            : 1500,
                                              wapperSelector      : wrapper_selector,
                                              navItemsSelector    : nav_items_selector,
-                                             navOuterSelector    : '#navigator-outer',
+                                             navOuterSelector    : nav_outer_selector,
                                              isPreloaded         : false,
 			                                 maxItemDisplay	 	 : max_item_display,
                                              navigatorHeight     : nav_height,
                                              navigatorWidth      : nav_width,
+                                             navPosition         : nav_position,
                                              auto                : true,
                                              caption             : '.promo-item-desc',
                                              opacityClass        : 'lof-opacity',
