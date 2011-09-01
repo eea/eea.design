ee79106d8734897be2570543a6feaf6bb66976e5
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-01 15:03:13 +0000

    Refs #8487; WIP filtering the items when changing topics fron the dropdown

--- trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_js/whatsnew_gallery.js
+++ trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_js/whatsnew_gallery.js
@@ -1,35 +1,14 @@
 (function($) {
     $(function() {
         $("#highlights-high, #highlights-middle").tabs("div.highlightMiddle", {tabs: 'div.panel', effect: 'slide'});
-        $("ul#tabs").tabs("> .highlights");
-
-        var host = window.location.host, http = 'http://',
-            localhost = host.indexOf('localhost') != '-1' ? true : undefined,
-            site_address = localhost ? http + host + '/www/' : http + host + '/';
-
-        $topic_selector = $("#topic-selector");
-        $topic_selector.find('[value="default"]').remove();
-        $topic_selector.change(
-            function displayResult() {
-                var x = this.selectedIndex,
-                    y = this.options;
-                var sel_value = y[x].value,
-                    sel_text = y[x].innerText;
-                var cur_tab_val = $("#tabs a.current")[0].id.substr(4);
-                var news = $(".highlights").filter( function(index) {
-                     return this.style.display === "block";
-                });
+        var whatsnew_func = function(cur_tab_val, sel_text, sel_value) {
+                var news = $("#whatsnew-gallery").find(".highlights:visible"); 
                 var address = site_address + cur_tab_val + "_gallery_macro";
+
                 // filter by topic notice
-                var filter_topic = news.find(".filter-topic"),
+                var filter_topic = news.find(".filter-topic")[0],
                     filter_topic_text = "Filtered by <span>" + sel_text + "</span> topic";
-                if (sel_value) {
-                    filter_topic[0].innerHTML = filter_topic_text;
-                } 
-                else {
-                    filter_topic[0].innerHTML = "";
-                
-                }
+                filter_topic.innerHTML = sel_value ? filter_topic_text : "";
 
                 var no_results = $("<div class='portalMessage informationMessage'><p>No results are available for this topic</p></div>");
                 var gallery_ajax = $(".gallery-ajax", news);
@@ -51,6 +30,67 @@
                     }
 
                 });
+        };
+        $("ul#tabs").tabs("> .highlights", function(event, index) {
+        var host = window.location.host, http = 'http://',
+            localhost = host.indexOf('localhost') != '-1' ? true : undefined,
+            site_address = localhost ? http + host + '/www/' : http + host + '/';
+            var cur_tab = this.getTabs()[index];
+            cur_tab.data = cur_tab.data || "none";
+            var sel_value = $("#topic-selector").find(":selected").val();
+            if (sel_value) {
+                if (cur_tab.data !== sel_value) {
+                    cur_tab.data = sel_value;
+                var cur_tab_val = cur_tab.id.substr(4);
+                // WIP this function is a repetition of the whatsnew_function
+                // but the onBeforeClick event of the tabs doesn't take in the 
+                // whatsnew_function correctly
+                var inner_func = function() {
+                    var address = site_address + cur_tab_val + "_gallery_macro";
+                    var no_results = $("<div class='portalMessage informationMessage'><p>No results are available for this topic</p></div>");
+                    var news = $("#whatsnew-gallery").find(".highlights:visible"); 
+                var filter_topic = news.find(".filter-topic")[0],
+                    filter_topic_text = "Filtered by <span>" + sel_text + "</span> topic";
+                    filter_topic.innerHTML = sel_value ? filter_topic_text : "";
+                    var gallery_ajax = $(".gallery-ajax", news);
+                    var layout_selection = $('.gallery-layout-selection li a', news)[0];
+                    gallery_ajax.load( address, {topic: sel_value, tab: cur_tab.data },  function(html) {
+                        if (html.length > 1) {
+                            if (layout_selection.className === "list-layout active-list"){
+                                gallery_ajax.find('.gallery-album').addClass('hiddenStructure');
+                            }
+                            else {
+                                gallery_ajax.find('.gallery-listing').addClass('hiddenStructure');
+                            }
+                            if (cur_tab_val === "multimedia") {
+                                $.getScript(site_address + "eea-mediacentre.js");
+                            }
+                        }
+                        else {
+                           no_results.appendTo(gallery_ajax);
+                        }
+
+                    });
+                };
+                window.setTimeout(inner_func, 50);
+                }
+            }
+        });
+
+        var host = window.location.host, http = 'http://',
+            localhost = host.indexOf('localhost') != '-1' ? true : undefined,
+            site_address = localhost ? http + host + '/www/' : http + host + '/';
+
+        $topic_selector = $("#topic-selector");
+        $topic_selector.find('[value="default"]').remove();
+        $topic_selector.change(
+            function displayResult() {
+                var x = this.selectedIndex,
+                    y = this.options;
+                var topic_value = y[x].value,
+                    topic_text = y[x].innerText;
+                var tab_val = $("#tabs a.current")[0].id.substr(4);
+                whatsnew_func(cur_tab_val = tab_val, sel_text = topic_text, sel_value = topic_value);
             }
         );
 
@@ -90,3 +130,4 @@
     });
 
 })(jQuery);
+
