c768670f3f1da69d393943e429d985398cb2d3a4
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-13 14:26:56 +0000

    cosmetics in being consistent and use only this instead of the old term

--- trunk/eea.design/branches/plone4/eea/design/skins/eea_design/global_searchbox.js
+++ trunk/eea.design/branches/plone4/eea/design/skins/eea_design/global_searchbox.js
@@ -3,19 +3,18 @@
         var search_forms = $("#portal-searchbox, #visual-column-wrapper").find(".searchforms");
         var text_inputs = search_forms.find("input:text");
         text_inputs.each( function() {
-                var term = this;
                 var search_label = this.title + "...";
-                term.onfocus = function() {
+                this.onfocus = function() {
                     if (this.value == search_label) {
                         this.value = "";
                     }
                 };
-                term.onblur = function() {
+                this.onblur = function() {
                     if (this.value === "") {
                         this.value = search_label;
                     }
                 };
-                term.value = search_label;
+                this.value = search_label;
         });
 
     });
