a897645bd1db4fd6ff6f3c9c65b375614587022c
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-22 11:45:29 +0000

    Fixed IE compatibility.

--- trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_cms/patch_keyword_widget.js
+++ trunk/eea.design/branches/plone4/eea/design/skins/eeadesign_cms/patch_keyword_widget.js
@@ -1,10 +1,9 @@
 // Patch keyword widget so that "New tags" and "Existing tags" will not overlap
 
 jQuery(document).ready(function ($) {
-    var keyword_widget = $("#fieldset-categorization #archetypes-fieldname-subject div")[2];
-    keyword_widget.style.cssText = '';
+    var keyword_widget = $("#fieldset-categorization #archetypes-fieldname-subject div");
+    keyword_widget.attr('style', '');
 
-    var textarea_widget = keyword_widget.getElementsByTagName('textarea')[0];
-    textarea_widget.rows = 3;
+    var textarea_widget = jQuery('textarea', keyword_widget);
+    textarea_widget.attr('rows', 3);
 });
-
