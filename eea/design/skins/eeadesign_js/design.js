ddca44f139343f746298f28397744424d8df9a8b
ichim-david <ichim.david@gmail.com>
2019-08-15 17:49:30 +0300

    Refs #108460 header jump and faceted navigation photoAlbumEntry fixes

--- eea/design/skins/eeadesign_js/design.js
+++ eea/design/skins/eeadesign_js/design.js
@@ -37,6 +37,10 @@ jQuery(document).ready(function($) {
     appendTo($related_items, $center_bottom_area);
     appendTo($socialmedia, $center_bottom_area);
 
+    if ($("#right-area").length) {
+        $portal_column_content.removeClass('width-full');
+    }
+
     // hide element if empty or has less than on equal to given
     // child length
     var hide_empty_container = function($el, child_count, $checked_el) {
