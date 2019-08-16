5dce1cfb91384748af446b98b075a874cdbb5891
ichim-david <ichim.david@gmail.com>
2019-08-16 14:51:00 +0300

    Refs #108460 remove also cell class:
    - with it IE11 has a large column width on frontpage

--- eea/design/skins/eeadesign_js/design.js
+++ eea/design/skins/eeadesign_js/design.js
@@ -29,7 +29,7 @@ jQuery(document).ready(function($) {
     appendTo($socialmedia, $column_area);
     if ($column_area.length) {
         appendTo($viewlet_below_content, $column_area);
-        $portal_column_content.removeClass('width-full');
+        $portal_column_content.removeClass('cell width-full');
     }
     else {
         appendTo($viewlet_below_content, $content);
@@ -38,7 +38,7 @@ jQuery(document).ready(function($) {
     appendTo($socialmedia, $center_bottom_area);
 
     if ($("#right-area").length) {
-        $portal_column_content.removeClass('width-full');
+        $portal_column_content.removeClass('cell width-full');
     }
 
     // hide element if empty or has less than on equal to given
