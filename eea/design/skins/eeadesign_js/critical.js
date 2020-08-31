2d1af7565d38605d64b2d26db06862a71eac3496
David Ichim <ichim.david@gmail.com>
2020-08-31 11:29:28 +0300

    Refs #120363 insert also menu for root that is present

--- eea/design/skins/eeadesign_js/critical.js
+++ eea/design/skins/eeadesign_js/critical.js
@@ -15,31 +15,33 @@ var make_nav_blend = function() {
 
   var portlet_nav = qs(".portletNavigationTree");
   var extra_nav_li = document.createElement('li');
-  extra_nav_li.className = "mobile-only";
+  var level = 1;
+  extra_nav_li.className = "mobile-only pl-" + level;
   var cloned_portlet_nav = portlet_nav.cloneNode(true);
   extra_nav_li.appendChild(cloned_portlet_nav);
-  var values = new window.Map();
-  var nav_header = portlet_nav.querySelector("#firstHeader").querySelector("a");
-  var nav_header_text = nav_header.innerText;
-  var nav_header_href = nav_header.href;
+  var nav_header = cloned_portlet_nav.querySelector("#firstHeader");
+  var nav_header_link = nav_header.querySelector("a");
+  var nav_header_text = nav_header_link.innerText;
+  var nav_header_href = nav_header_link.href;
   var global_nav = qs("#portal-globalnav");
   var global_nav_root_link = Array.prototype.filter.call(global_nav.querySelectorAll("a"), function(item) {
     return nav_header_href.indexOf(item.href) !== -1;
   });
   var global_nav_root_li;
   // var $selected_nav_link = portlet_nav.querySelector(".navTreeCurrentItem");
-  // var level = 0;
-  // var insert_last = false;
   if (!global_nav_root_link.length) {
-    // insert_last = true;
-    values.set(nav_header_text, { href: nav_header.href });
+    global_nav_root_li = document.createElement("li");
+    global_nav_root_li.className = "mobile-only";
+    global_nav_root_li.appendChild(nav_header_link);
+    cloned_portlet_nav.removeChild(nav_header);
+    global_nav.appendChild(global_nav_root_li)
+    global_nav_root_li.insertAdjacentElement('afterend', extra_nav_li);
   } else {
     global_nav_root_link = global_nav_root_link[0];
     global_nav_root_li = global_nav_root_link.parentNode;
     global_nav_root_li.className = "";
     level += 1;
     if (global_nav_root_link.href !== nav_header_href) {
-      values.set(nav_header_text, { href: nav_header_href });
       global_nav_root_li.insertAdjacentElement('afterend', extra_nav_li);
     }
   }
